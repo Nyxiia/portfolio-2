de33cdcfea2c35e4b21be35ff1c274f5a177eeb1
Nyxiia <92077239+Nyxiia@users.noreply.github.com>
2023-07-27 19:39:51 +0000

    Initial commit

---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?display=swap&family=Roboto:ital,wght@0,100;0,300;0,700;1,100;1,300;1,700&family=Delicious+Handrawn:ital,wght@0,400;1,400"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
